ef030ec5b98f6d2a8a14f64a383ccbfd5c9f6b7a
yangjh <yangjh@morningtec.cn>
2017-01-04 20:01:16 +0800

    添加下拉刷新
    目前还差一个良好的加载更多的方案

--- demopullrefresh/src/main/java/mtp/morningtec/com/demopullrefresh/MainActivity.java
+++ demopullrefresh/src/main/java/mtp/morningtec/com/demopullrefresh/MainActivity.java
@@ -2,12 +2,100 @@ package mtp.morningtec.com.demopullrefresh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import yang.developtools.toollibrary.common.widget.pullrefresh.PtrClassicDefaultHeader;
+import yang.developtools.toollibrary.common.widget.pullrefresh.PtrClassicFrameLayout;
+import yang.developtools.toollibrary.common.widget.pullrefresh.PtrDefaultHandler;
+import yang.developtools.toollibrary.common.widget.pullrefresh.PtrFrameLayout;
+import yang.developtools.toollibrary.common.widget.pullrefresh.PtrHandler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PtrFrameLayout pullLayout;
+    private PtrClassicFrameLayout mPtrFrame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        demoPtrClassicFrameLayout();//有默认刷新头部的
+        demoPtrFrameLayout();//没有默认刷新头部的
+    }
+
+
+    private void demoPtrClassicFrameLayout(){
+        mPtrFrame = (PtrClassicFrameLayout)findViewById(R.id.material_style_ptr_frame);
+
+
+
+        mPtrFrame.setLastUpdateTimeRelateObject(this);
+        mPtrFrame.setPtrHandler(new PtrHandler() {
+            @Override
+            public void onRefreshBegin(PtrFrameLayout frame) {
+
+            }
+
+            @Override
+            public boolean checkCanDoRefresh(PtrFrameLayout frame, View content, View header) {
+                return PtrDefaultHandler.checkContentCanBePulledDown(frame, content, header);
+            }
+        });
+        // the following are default settings
+        mPtrFrame.setResistance(1.7f);
+        mPtrFrame.setRatioOfHeaderHeightToRefresh(1.2f);
+        mPtrFrame.setDurationToClose(200);
+        mPtrFrame.setDurationToCloseHeader(1000);
+        // default is false
+        mPtrFrame.setPullToRefresh(false);
+        // default is true
+        mPtrFrame.setKeepHeaderWhenRefresh(true);
+        mPtrFrame.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // mPtrFrame.autoRefresh();
+            }
+        }, 100);
+    }
+
+
+    private void demoPtrFrameLayout(){
+        pullLayout = (PtrFrameLayout)findViewById(R.id.material_style_ptr_frame);
+        initPull();
+        // 阻尼系数
+        pullLayout.setResistance(1.7f);
+        pullLayout.setRatioOfHeaderHeightToRefresh(1.2f);//多少倍头部视图的高度后可以刷新
+        pullLayout.setDurationToClose(200);//滚回刷新中的高度所用时间
+        pullLayout.setDurationToCloseHeader(1000);//刷新完成后缩回去所用时间
+// default is false
+        pullLayout.setPullToRefresh(false);//能否下拉刷新
+// default is true
+        pullLayout.setKeepHeaderWhenRefresh(true);//刷新时保持头部可见
+    }
+
+    private void initPull(){
+        PtrClassicDefaultHeader header = new PtrClassicDefaultHeader(this);
+        pullLayout.addPtrUIHandler(header);//设置下拉的数据监听
+        pullLayout.setHeaderView(header);//设置默认的下拉头部
+        pullLayout.setPtrHandler(new PtrHandler() {
+            @Override
+            public void onRefreshBegin(PtrFrameLayout frame) {
+//                updateData();
+                pullLayout.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        pullLayout.refreshComplete();
+                    }
+                },3000);
+            }
+
+            @Override
+            public boolean checkCanDoRefresh(PtrFrameLayout frame, View content, View header) {
+                return PtrDefaultHandler.checkContentCanBePulledDown(frame, content, header);
+            }
+        });
+        //下拉时是否保持内部布局不下拉
+//        pullLayout.setPinContent(true);
     }
 }
--- toollibrary/src/main/java/yang/developtools/toollibrary/base/widget/BasePulltoRefreshView.java
+++ toollibrary/src/main/java/yang/developtools/toollibrary/base/widget/BasePulltoRefreshView.java
@@ -11,17 +11,32 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import yang.developtools.toollibrary.R;
+import yang.developtools.toollibrary.common.widget.listview.SwipeMenuRecyclerView;
+import yang.developtools.toollibrary.common.widget.pullrefresh.PtrClassicDefaultHeader;
+import yang.developtools.toollibrary.common.widget.pullrefresh.PtrDefaultHandler;
+import yang.developtools.toollibrary.common.widget.pullrefresh.PtrFrameLayout;
+import yang.developtools.toollibrary.common.widget.pullrefresh.PtrHandler;
+import yang.developtools.toollibrary.common.widget.pullrefresh.PtrUIHandler;
 
 /**
  * Created by yanjh on 2016/12/21 0021.
  * 一个基础的用于下拉刷新，上拉加载更多的控件
  */
 
-public class BasePulltoRefreshView extends LinearLayout{
+public abstract class BasePulltoRefreshView extends LinearLayout{
+
+    private PtrFrameLayout mPullRefreshLayout;//下拉刷新的载体
+    private SwipeMenuRecyclerView mCustomRecyclerView;//左滑右划以及长按拖动的载体
+
+    private View header;//下拉刷新时头部的视图
 
 
-    private SwipeRefreshLayout mSwipeRefreshLayout;
-    private RecyclerView mRecyclerView;
+    private int currentPage = 0;//当前页码
+    private int pageSize = 20;//每页的数量
+
+
+//    private SwipeRefreshLayout mSwipeRefreshLayout;
+//    private RecyclerView mRecyclerView;
 
     public BasePulltoRefreshView(Context context) {
         super(context);
@@ -48,11 +63,45 @@ public class BasePulltoRefreshView extends LinearLayout{
      * 初始化
      */
     private void initView(Context context){
-        LayoutInflater.from(context).inflate(R.layout.widget_pull_to_refresh,this,true);
+        View contentView = LayoutInflater.from(context).inflate(R.layout.widget_pull_to_refresh,this,true);
+        mPullRefreshLayout = (PtrFrameLayout)contentView.findViewById(R.id.mPullRefreshLayout);
+        mCustomRecyclerView = (SwipeMenuRecyclerView)contentView.findViewById(R.id.mCustomRecyclerView);
+
+        initPullRefresh();
+        initScroll();
     }
 
-    private void initView(){
+    /**
+     * 初始化下拉刷新的效果等
+     */
+    private void initPullRefresh(){
+        initPull();
+        // 阻尼系数
+        mPullRefreshLayout.setResistance(1.7f);
+        mPullRefreshLayout.setRatioOfHeaderHeightToRefresh(1.2f);//多少倍头部视图的高度后可以刷新
+        mPullRefreshLayout.setDurationToClose(200);//滚回刷新中的高度所用时间
+        mPullRefreshLayout.setDurationToCloseHeader(1000);//刷新完成后缩回去所用时间
+// default is false
+        mPullRefreshLayout.setPullToRefresh(false);//能否下拉刷新
+// default is true
+        mPullRefreshLayout.setKeepHeaderWhenRefresh(true);//刷新时保持头部可见
+    }
 
+    private void initPull() {
+        header = initHeadView();
+        mPullRefreshLayout.addPtrUIHandler((PtrUIHandler)header);//设置下拉的数据监听
+        mPullRefreshLayout.setHeaderView(header);//设置默认的下拉头部
+        mPullRefreshLayout.setPtrHandler(new PtrHandler() {
+            @Override
+            public void onRefreshBegin(PtrFrameLayout frame) {
+                refreshData();
+            }
+
+            @Override
+            public boolean checkCanDoRefresh(PtrFrameLayout frame, View content, View header) {
+                return PtrDefaultHandler.checkContentCanBePulledDown(frame, content, header);
+            }
+        });
     }
 
     /**
@@ -67,13 +116,13 @@ public class BasePulltoRefreshView extends LinearLayout{
      * 这样可以解决
      */
     private void scrollProblem(){
-        if(null == mRecyclerView || null == mSwipeRefreshLayout)return;
-        mRecyclerView.setOnScrollListener(new RecyclerView.OnScrollListener(){
+        if(null == mCustomRecyclerView || null == mCustomRecyclerView)return;
+        mCustomRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener(){
             @Override
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
                 int topRowVerticalPosition =
                         (recyclerView == null || recyclerView.getChildCount() == 0) ? 0 : recyclerView.getChildAt(0).getTop();
-                mSwipeRefreshLayout.setEnabled(topRowVerticalPosition >= 0);
+                mCustomRecyclerView.setEnabled(topRowVerticalPosition >= 0);
             }
 
             @Override
@@ -82,4 +131,38 @@ public class BasePulltoRefreshView extends LinearLayout{
             }
         });
     }
+
+    /**
+     * 刷新数据
+     */
+    public abstract void refreshData();
+
+    /**
+     * 加载数据
+     * @param page          页码
+     * @param pageSize      每页数据数量
+     */
+    public abstract void loadData(int page,int pageSize);
+
+    /**
+     * 刷新结束
+     */
+    public void refreshComplete(){
+        if(null == mPullRefreshLayout)return;
+        try{
+            mPullRefreshLayout.refreshComplete();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 初始化下拉刷新的头部View
+     * 如果需要自定义，需要继承了  PtrUIHandler接口的View
+     * @return
+     */
+    public View initHeadView(){
+        return new PtrClassicDefaultHeader(getContext());
+    }
+
 }
